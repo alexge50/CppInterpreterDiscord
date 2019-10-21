af07355bbfd7b03539f774cdd6c4b271b6f76ee3
alexge50 <alexge50.x@gmail.com>
2019-10-21 13:59:41 +0300

    added cling jail

--- cpp-interp.py
+++ cpp-interp.py
@@ -4,27 +4,38 @@ from discord.ext import commands
 import subprocess
 import re
 
+
 def cling(code, args):
-    result = subprocess.Popen(['cling', *args],
+    cling_jail = [
+        'nsjail',
+        '--config', 'interpreter-jail.cfg',
+        '--really_quiet',
+        '--', '/usr/bin/cling', *args]
+    result = subprocess.Popen(cling_jail,
                               stdin=subprocess.PIPE,
                               stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE).communicate(code.encode())
-    return (result[0].decode()[177:].strip(), result[1].decode().strip())
+    return result[0].decode()[177:].strip(), result[1].decode().strip()
+
 
 bot = commands.Bot(command_prefix='>')
+
 
 @bot.event
 async def on_ready():
     print('running as {0.user}'.format(bot))
 
+
 @bot.command()
 async def eval(ctx, *, args):
     stdout, stderr = cling(args, ['-std=c++17'])
+    print(stdout, stderr)
 
     if stderr == '':
         await ctx.send(f'```cpp\n{stdout}\n```')
     else:
         await ctx.send(f'```\n{stderr}\n```')
+
 
 @bot.command()
 async def eval_block(ctx, *, args):
@@ -36,6 +47,7 @@ async def eval_block(ctx, *, args):
     code_block = code_block[:-3]
 
     stdout, stderr = cling(code_block, cli_arguments.split(' '))
+    print(stdout, stderr)
 
     if stderr == '':
         await ctx.send(f'```cpp\n{stdout}\n```')
