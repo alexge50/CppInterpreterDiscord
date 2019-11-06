d083be94d4eb6d149ee70a926d648340026fa872
alexge50 <alexge50.x@gmail.com>
2019-11-06 13:51:15 +0200

    added interpreters module

---
+++ interpreters.py
@@ -0,0 +1,23 @@
+import nsjail
+from collections import namedtuple
+
+InterpreterOutput = namedtuple('InterpreterOutput', 'stdout stderr return_code')
+
+
+def cling(code, args) -> InterpreterOutput:
+    log_entries, executed_program = nsjail.run_process(
+        'interpreter-jail.cfg',
+        '/opt/cling/bin/cling',
+        code,
+        args)
+
+    print(log_entries)
+
+    stdout = executed_program.stdout[177:].strip()
+    stderr = executed_program.stderr.strip()
+
+    return InterpreterOutput(
+        stdout=None if stdout == '' else stdout,
+        stderr=None if stderr == '' else stderr,
+        return_code=executed_program.returncode
+    )
