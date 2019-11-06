9e5fb14dec908b635c1c4b219caec027276a2b18
alexge50 <alexge50.x@gmail.com>
2019-11-06 13:51:25 +0200

    updated main file

--- cpp-interp.py
+++ cpp-interp.py
@@ -1,22 +1,9 @@
+import re
+
 import discord
 from discord.ext import commands
 
-import subprocess
-import re
-
-
-def cling(code, args):
-    cling_jail = [
-        'nsjail',
-        '--config', 'interpreter-jail.cfg',
-        '--really_quiet',
-        '--', '/usr/bin/cling', *args]
-    result = subprocess.Popen(cling_jail,
-                              stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE).communicate(code.encode())
-    return result[0].decode()[177:].strip(), result[1].decode().strip()
-
+import interpreters
 
 bot = commands.Bot(command_prefix='>')
 
@@ -28,31 +15,40 @@ async def on_ready():
 
 @bot.command()
 async def eval(ctx, *, args):
-    stdout, stderr = cling(args, ['-std=c++17'])
-    print(stdout, stderr)
+    stdout, stderr, return_code = interpreters.cling(args, ['-std=c++17'])
 
-    if stderr == '':
-        await ctx.send(f'```cpp\n{stdout}\n```')
-    else:
-        await ctx.send(f'```\n{stderr}\n```')
+    message = f'Return Code: {return_code}\n'
+
+    if stdout is not None:
+        message += f'stdout:\n```cpp\n{stdout}\n```'
+
+    if stderr is not None:
+        message += f'stderr:\n```fix\n{stderr}\n```'
+
+    await ctx.send(message)
 
 
 @bot.command()
 async def eval_block(ctx, *, args):
-    code_pattern = re.compile('```(cpp)?[\s\S]*```')
+    code_pattern = re.compile(r'```(cpp)?[\s\S]*```')
     cli_arguments = code_pattern.sub('', args).strip()
     code_block = code_pattern.search(args).group()
 
     code_block = code_block[6:] if code_block[:6] == '```cpp' else code_block[3:]
     code_block = code_block[:-3]
 
-    stdout, stderr = cling(code_block, cli_arguments.split(' '))
-    print(stdout, stderr)
+    stdout, stderr, return_code = interpreters.cling(code_block, cli_arguments.split(' '))
 
-    if stderr == '':
-        await ctx.send(f'```cpp\n{stdout}\n```')
-    else:
-        await ctx.send(f'```\n{stderr}\n```')
+    message = f'Return Code: {return_code}\n'
+
+    if stdout is not None:
+        message += f'stdout:\n```cpp\n{stdout}\n```'
+
+    if stderr is not None:
+        message += f'stderr:\n```fix\n{stderr}\n```'
+
+    await ctx.send(message)
+
 
 with open('token', 'r') as f:
     token = f.read()
